c73abcb54423c3e9bab962db958bc3b588967892
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-02-26 17:22:02 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Winform_Market_Project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
